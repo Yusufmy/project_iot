daff1c44fd0755da92cb1378752d090221fdb88e
alfinputra013 <putraalfinmulya@gmail.comm>
2023-09-11 14:34:31 +0700

    git

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:from_css_color/from_css_color.dart';
+// import 'package:from_css_color/from_css_color.dart';
 import 'package:iot_project/onboarding_page.dart';
 
 class SplashScreen extends StatefulWidget {
